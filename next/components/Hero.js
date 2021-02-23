e50a3e142c3e4638196d9df1f90bac3f60b2b6d2
james <jguest777@gmail.com>
2021-02-23 14:58:46 -0500

    added hero overlay and logo with frosted effect

--- next/components/Hero.js
+++ next/components/Hero.js
@@ -1,3 +1,9 @@
 export default function Hero() {
-	return <div className='hero'></div>
+	return (
+		<div className='hero'>
+			<div className='hero__overlay'>
+				<img src='/logo.png' alt='Tops logo' />
+			</div>
+		</div>
+	)
 }
